604ae435ea744936889a634a22db805918bc1bdf
lisianthus <lisianthus623@gmail.com>
2020-02-05 18:57:14 +0700

    delete useless comment

--- backend/article/models.py
+++ backend/article/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-
-# Create your models here.
 class Article(models.Model) :
     article_author  = models.ForeignKey(User, on_delete = models.CASCADE)
     article_title   = models.CharField(max_length = 30 )
