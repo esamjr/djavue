67985cc05378357964c9fdc5147054273621673c
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-05 17:22:36 +0700

    Added home page

---
+++ src/components/Post.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full md:w-1/2 p-6 m-6 bg-white shadow-md rounded-lg" :class="grid">
+    <img :src="item.image" class="rounded-lg" />
+    <h1 class="text-5xl font-bold">
+      <router-link :to="'/post/' + item.id">{{ item.title }}</router-link>
+    </h1>
+    <p>{{ item.content }}</p>
+    <div class="pt-12">
+      <p class="text-gray-600">By {{ item.author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["grid", "item"]
+};
+</script>
+
+<style>
+</style>
