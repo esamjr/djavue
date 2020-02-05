c18d482a672baed151823fba5446b684bdc67c66
lisianthus <lisianthus623@gmail.com>
2020-02-05 21:57:47 +0700

    show current date

--- backend/article/views.py
+++ backend/article/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.timezone import get_current_timezone
 import sys, datetime
 
 def default_view(request) :
     date = datetime.datetime.now()
-    html = "<h3>[ Work Perfectly ]</h3><span>Date : %s</span>" % date
+    # origin_date = date.strftime("%F, %j %D %Y")
+    origin_date = date.strftime("%B, %a %d %Y")
+
+    html = f"<h3>[ Work Perfectly ]</h3><span>Date : {origin_date}</span>"
+    
     return HttpResponse(html)
