07ae883e55125a6e80754788467e144d85bc36d4
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-05 20:03:57 +0700

    Removed trialing whitespaces and added Procfile for heroku django deployment

--- pkl_blog/settings.py
+++ pkl_blog/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     # Third Party
     'article.apps.ArticleConfig',
 ]
