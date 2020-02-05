70f77405cb33954357d429bf876bf8e82b56ab07
lisianthuss116 <59347925+lisianthuss116@users.noreply.github.com>
2020-02-05 22:29:12 +0700

    Update settings.py

--- pkl_blog/settings.py
+++ pkl_blog/settings.py
@@ -30,9 +30,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-  
+    
     # Installed apps
     'article.apps.ArticleConfig',
-
+    
     # third party
     'rest_framework',
 ]
@@ -89,10 +87,7 @@ DATABASES = {
     }
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -114,22 +109,12 @@ REST_FRAMEWORK = {
     ]
 }
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
