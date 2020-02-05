6dd96638720d592c7acfc37b05fd303c7abdba4f
lisianthus <lisianthus623@gmail.com>
2020-02-05 20:42:03 +0700

    create default view

--- backend/article/views.py
+++ backend/article/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import sys, datetime
 
-# Create your views here.
+def default_view(request) :
+    date = datetime.datetime.now()
+    html = "<h3>[ Work Perfectly ]</h3><span>Date : %s</span>" % date
+    return HttpResponse(html)
