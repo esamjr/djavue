f6aa78bea00d9460524d6ef22c4cb6764697608e
lisianthus <lisianthus623@gmail.com>
2020-02-05 18:58:13 +0700

    article view json

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .serializers import ArticleSerializer
+from rest_framework import viewsets
+import sys
+sys.path.append('../')
+from article.models import Article
 
-# Create your views here.
+class ArticleViewSet(viewsets.ModelViewSet) :
+    """
+    API endpoint that allows article to be viewen or edited
+    """
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
