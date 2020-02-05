6315230e3dfdbc39a7582462cc0a94086ed12c34
lisianthus <lisianthus623@gmail.com>
2020-02-05 18:59:31 +0700

    create url for api

--- pkl_blog/urls.py
+++ pkl_blog/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+import sys, re
+sys.path.append('backend/')
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'articles', views.ArticleViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Api
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
 ]
