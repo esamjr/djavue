76ff04e5b99b0c56b310b048cce169cd93e790b5
lisianthus <lisianthus623@gmail.com>
2020-02-05 17:36:53 +0700

    fix bug #001 | cannot import apps

--- pkl_blog/settings.py
+++ pkl_blog/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
-from backend import article
+import os, sys
+sys.path.append('backend/')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
