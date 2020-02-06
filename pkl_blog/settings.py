b4a8d5b208055181b7b5cbc6ab22e32be5f4bb45
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-07 00:02:58 +0700

    Fixed incorrect constant

--- pkl_blog/settings.py
+++ pkl_blog/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/dist/'
 
 STATICFILES_DIRS = [
