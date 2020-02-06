6d3d4b8278a9959d7bbcd2fbb257dd8fff51fd30
Achmad Dzulfian <achmad.dzulfian7@gmail.com>
2020-02-06 22:12:28 +0700

    Adding create and edit form (#20)
    * Adding create and edit form
    
    * modified index.js to be lazy load

---
+++ src/views/Create.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home container mx-auto">
+    <h1 class="text-5xl font-bold">Create Article</h1>
+    <div class="flex flex-wrap">
+        
+    </div>
+    <form-app></form-app>
+  </div>
+</template>
+
+<script>
+import FormCreateVue from '../components/FormCreate.vue'
+
+export default {
+    name: "create",
+    components: {
+        "form-app": FormCreateVue
+    }
+}
+</script>
