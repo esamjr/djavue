c729c11497f341dafa3b0e6cb2e15599f5a219ab
lisianthus <lisianthus623@gmail.com>
2020-02-06 22:47:41 +0700

    change render default-view

--- pkl_blog/default.py
+++ pkl_blog/default.py
@@ -1,10 +1,6 @@
-from django.utils.timezone import get_current_timezone
-from django.http import HttpResponse
-import datetime
+from django.shortcuts import render
+import datetime, sys
+sys.path.append('../dist/template/')
 
 def default_view(request) :
-    date = datetime.datetime.now()
-    origin_date = date.strftime("%B, %a %d %Y")
-
-    html = f"<h3>[ Work Perfectly ]</h3><span>Date : {origin_date}</span>"
-    return HttpResponse(html)
+    return render(request, 'index.html')
