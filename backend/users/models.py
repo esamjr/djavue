1db26e15b0313d00cc1aa226006bd0937185503c
lisianthus <lisianthus623@gmail.com>
2020-02-06 21:42:01 +0700

    delete user fields

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,10 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model) :
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    username = models.CharField(
-        default = 'New user',
-        max_length = 60)
+    username = models.CharField(default = 'New user', max_length = 60)
     
     def __str__(self) :
         return f'{self.username} Profile'
