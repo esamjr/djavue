4e211216052a8f8eccd5796ba004997a0d2ac49f
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-07 23:56:18 +0700

    Delete unused constant

--- pkl_blog/settings.py
+++ pkl_blog/settings.py
@@ -18,7 +18,6 @@ sys.path.append('backend/')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '+tbk32+*%pf&1vrg$8a504k1dc368#l_hcz0hw^)kylt^*t#ge'
