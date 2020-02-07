d16d7ac99c3560b19d715c9f6b3960712370c82e
lisianthuss116 <59347925+lisianthuss116@users.noreply.github.com>
2020-02-07 18:42:28 +0700

    update coalescing url

--- pkl_blog/settings.py
+++ pkl_blog/settings.py
@@ -87,7 +87,8 @@ port     = os.getenv('PORT')
 db       = os.getenv('DB_NAME')
 
 DATABASES    = {}
-DATABASE_URL = 'postgres://{0}:{1}@{2}/{3}'.format(user, password, port, db) or os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL') or 'postgres://{0}:{1}@{2}/{3}'.format(
+    user, password, port, db)
 DATABASES['default'] = dj_database_url.parse(DATABASE_URL, conn_max_age = None)
 
 # Password validation
