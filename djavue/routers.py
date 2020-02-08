0060d9aa9f2eda979cdc8eff68bb1f646d04f060
lisianthus <lisianthus623@gmail.com>
2020-02-08 19:06:20 +0700

    CHANGE | all routes for api

--- djavue/routers.py
+++ djavue/routers.py
@@ -1,9 +1,24 @@
-from api.viewsets import (
-    UserViewSet, ArticleViewSet)
+from django.urls import path
 import sys
-from rest_framework import routers
 sys.path.append('../backend/')
+from api.viewsets import (
+    ArticleView, ArticleUpdateView, ArticleCreateView, ArticleDeleteView, 
+    UserView)
 
-router = routers.DefaultRouter()
-router.register('users', UserViewSet)
-router.register('articles', ArticleViewSet)
+article = 'articles'
+user    = 'users'
+
+urlpatterns = [
+    # Get response
+    path('articles/', ArticleView.as_view()),
+    path('users/', UserView.as_view()),
+    
+    # Create
+    path('articles/create/', ArticleCreateView.as_view()),
+    
+    # Update
+    path('articles/<int:pk>', ArticleUpdateView.as_view()),
+    
+    # Delete
+    path('articles/delete/<int:pk>', ArticleDeleteView.as_view()),
+]
