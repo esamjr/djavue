dc9e6db8c65797a28a28174e692f39ce4161c641
lisianthus <lisianthus623@gmail.com>
2020-02-09 00:33:09 +0700

    USE | new route for api

--- djavue/routers.py
+++ djavue/routers.py
@@ -1,24 +1,11 @@
 from django.urls import path
-import sys
-sys.path.append('../backend/')
-from api.viewsets import (
-    ArticleView, ArticleUpdateView, ArticleCreateView, ArticleDeleteView, 
-    UserView)
+from backend.api.viewsets import get_post, update_delete
 
 article = 'articles'
 user    = 'users'
 
 urlpatterns = [
     # Get response
-    path('articles/', ArticleView.as_view()),
-    path('users/', UserView.as_view()),
-    
-    # Create
-    path('articles/create/', ArticleCreateView.as_view()),
-    
-    # Update
-    path('articles/<int:pk>', ArticleUpdateView.as_view()),
-    
-    # Delete
-    path('articles/delete/<int:pk>', ArticleDeleteView.as_view()),
+    path('articles/', get_post),
+    path('articles/<int:pk>', update_delete),
 ]
