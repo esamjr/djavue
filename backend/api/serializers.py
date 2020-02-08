e36149746b51c78e89f8b8df84b4ffb02c904c70
lisianthus <lisianthus623@gmail.com>
2020-02-09 00:30:45 +0700

    update Article-serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,8 @@
-from django.contrib.auth.models import User
-from django.utils.timezone import now
 from rest_framework import serializers
-from article.models import Article
 import datetime
-import sys
-sys.path.append('../')
+
+from django.contrib.auth.models import User
+from article.models import Article
 
 class UserSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=30)
@@ -14,28 +12,28 @@ class UserSerializer(serializers.ModelSerializer):
         return User.objects.create(**validated_data)
 
     def update(self, instance, validated_data) :
-        instance.user    = validated_data.get('username', instance.username)
-        instance.title   = validated_data.get('email', instance.email)
+        instance.user  = validated_data.get('username', instance.username)
+        instance.title = validated_data.get('email', instance.email)
         
         instance.save()
         return instance
 
-class ArticleSerializer(serializers.Serializer):
-    id      = serializers.ReadOnlyField(required=False)
-    user    = serializers.CharField(max_length=60)
-    title   = serializers.CharField(max_length=60)
+class ArticleSerializer(serializers.Serializer) :
+    id = serializers.ReadOnlyField(required=False)
+    user = serializers.CharField(max_length=60)
+    title = serializers.CharField(max_length=60)
     content = serializers.CharField(max_length=2000)
     date_posted = serializers.DateField(default=datetime.date.today)
     
     def create(self, validated_data) :
         return Article.objects.create(**validated_data)
 
-    def update(self, instance, validated_data) :
-        instance.id      = validated_data.get('id', instance.id)
-        instance.user    = validated_data.get('user', instance.user)
-        instance.title   = validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.date_posted = validated_data.get('date_posted', instance.date_posted)
+    def update(self,instance,validated_data) :
+        instance.id = validated_data.get('id',instance.id)
+        instance.user = validated_data.get('user',instance.user)
+        instance.title = validated_data.get('title',instance.title)
+        instance.content = validated_data.get('content',instance.content)
+        instance.date_posted = validated_data.get('date_posted',instance.date_posted)
         
         instance.save()
         return instance
