3fc579088cc704dbce37875ff9359db8b0eb34f9
lisianthus <lisianthus623@gmail.com>
2020-02-08 19:05:35 +0700

    change date-field

--- backend/article/models.py
+++ backend/article/models.py
@@ -2,12 +2,11 @@ from django.db import models
 from django.utils.timezone import now
 import datetime
 
-
 class Article(models.Model):
-    user = models.CharField(default='Anonymous', max_length=60)
-    title = models.CharField(max_length=60)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=now, editable=False)
+    user        = models.CharField(default='Anonymous', max_length=60)
+    title       = models.CharField(max_length=60)
+    content     = models.TextField()
+    date_posted = models.DateField(default=datetime.date.today, editable=False)
 
     def __str__(self):
         return self.title
