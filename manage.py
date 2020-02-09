ff4ba12aa6a521d5c4f7c10cca89aaeb3f94d001
lisianthus <lisianthus623@gmail.com>
2020-02-09 09:28:13 +0700

    use new exception

--- manage.py
+++ manage.py
@@ -2,30 +2,39 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import environ
+import dotenv
 
 def main() :
+    dotenv.read_dotenv(override = True)
+    env = environ.Env(DEBUG = (bool, False))
+    environ.Env.read_env('./.env')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', env('DJANGO_SETTINGS_MODULE'))
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        try :
+            import django
+        except ImportError :
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     try :
-        import environ
-        import dotenv
-        if True :
-            dotenv.read_dotenv(override = True)
-            env = environ.Env(DEBUG = (bool, False))
-            environ.Env.read_env('./.env')
-        
         main()
-    except :
-        raise Exception(
-            "hmm, somethin is wrong there. you need to fix it!"
-        )
+    except ImportError :
+        try :
+            import environ
+            import dotenv
+        except ImportError :
+            raise ImportError(
+                "Couldn't import environ and dotenv. Are you sure it's installed or are you forget to activate your virtual"
+                "virtual environment?"
+            )
+            
