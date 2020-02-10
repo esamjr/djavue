1280909a161a05df404ff3f809d76ff3b625f801
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-10 15:49:32 +0700

    Added bookmark button, updated article view

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,27 +1,51 @@
 <template>
-  <router-link
-    :to="'/post/' + article.id"
-    :class="grid"
-    class="w-full md:w-1/2 p-6 m-2 bg-white shadow-md rounded-lg"
-  >
-    <h1 class="text-4xl font-bold">{{ article.title }}</h1>
+  <div :class="grid" class="w-full md:w-1/2 p-6 m-2 bg-white shadow-md rounded-lg">
+    <h1 class="text-4xl font-bold">
+      <router-link :to="'/post/' + article.id">{{ article.title }}</router-link>
+    </h1>
     <p
       class="text-gray-500"
       v-if="article.content.length > 124"
     >{{ article.content.substring(0, 124) }}...</p>
     <p class="text-gray-500" v-else>{{ article.content }}</p>
-    <div class="pt-12">
-      <p class="text-gray-600">By {{ article.user }}</p>
-      <p class="text-gray-600">{{ article.date_posted }}</p>
+    <div class="flex justify-between items-baseline pt-12">
+      <span class>
+        <p class="text-gray-600">By {{ article.user }}</p>
+        <p class="text-gray-600">{{ article.date_posted }}</p>
+      </span>
+      <button @click="isBookmarked = !isBookmarked" class="bookmark-btn p-2 rounded-full hover:bg-teal-100 focus:outline-none">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          :fill="isBookmarked ? '#4FD1C5' : 'none'"
+          stroke="#4FD1C5"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-bookmark"
+        >
+          <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z" />
+        </svg>
+      </button>
     </div>
-  </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["grid", "article"]
+  props: ["grid", "article"],
+  data() {
+    return {
+      isBookmarked: false
+    }
+  }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.bookmark-btn {
+  transition: 0.2s;
+}
 </style>
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="home container mx-auto md:px-32 lg:px-64">
+  <div class="home container mx-auto md:px-32 lg:px-48">
     <div class="flex flex-wrap justify-center">
       <div class="w-full p-6 m-6" :class="grid">
-        <h1 class="text-5xl font-bold">{{ article.title }}</h1>
+        <h1 class="text-4xl font-bold">{{ article.title }}</h1>
         <p class="text-gray-600">By {{ article.user }}</p>
         <p class="text-gray-600">{{ article.date_posted }}</p>
         <div class="pt-12 content">{{ article.content }}</div>
@@ -36,7 +36,7 @@ export default {
           this.endpoint + this.$route.params["id"]
         );
 
-        this.article = response.data[0];
+        this.article = response.data;
       } catch (error) {
         console.log(error);
       }
