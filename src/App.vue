e9585cf45affffc9a452a0995a2b2ce60440ea2a
HotPotatoC <43059506+HotPotatoC@users.noreply.github.com>
2020-02-10 14:51:28 +0700

    Updated home page view

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,59 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link class="mx-4" to="/create">Create</router-link>
-      <router-link to="/edit">Edit</router-link>
+      <div class="upper p-5 bg-white shadow-md">
+        <div class="container mx-auto">
+          <div class="flex justify-between items-baseline">
+            <h1 class="text-2xl font-bold">
+              <router-link to="/" class="brand text-teal-400">Djavue</router-link>
+            </h1>
+            <div class="flex items-baseline">
+              <a href="#" class="px-2">
+                <img src="@/assets/icons/search.svg" alt />
+              </a>
+              <a href="#" class="px-2">
+                <img src="@/assets/icons/bookmark.svg" alt />
+              </a>
+              <a href="#" class="px-2 mr-5">
+                <img src="@/assets/icons/bell.svg" alt />
+              </a>
+              <button class="px-4 py-1 bg-transparent border-2 border-teal-400 text-teal-400 rounded focus:outline-none">
+                Upgrade
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom p-3 mb-12 bg-teal-100 shadow-md">
+        <div class="container mx-auto">
+          <div class="flex justify-around items-baseline text-gray-500">
+            <a href="#" class="text-sm text-teal-800 font-thin">HOME</a>
+            <a href="#" class="text-sm text-teal-800 font-thin">CATEGORY</a>
+            <a href="#" class="text-sm text-teal-800 font-thin">TRENDING</a>
+            <a href="#" class="text-sm text-teal-800 font-thin">SUBSCRIPTION</a>
+            <a href="#" class="text-sm text-teal-800 font-thin">CONTACT</a>
+          </div>
+        </div>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
+body {
   background: #f5f5f5;
+}
+
+#app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .brand {
+    font-family: "Gilroy", sans-serif;
   }
 }
 </style>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="w-full md:w-1/2 p-6 m-6 bg-white shadow-md rounded-lg" :class="grid">
-    <h1 class="text-5xl font-bold">
-      <router-link :to="'/post/' + article.id">{{ article.title }}</router-link>
-    </h1>
-    <p>{{ article.content }}</p>
+  <router-link
+    :to="'/post/' + article.id"
+    :class="grid"
+    class="w-full md:w-1/2 p-6 m-2 bg-white shadow-md rounded-lg"
+  >
+    <h1 class="text-4xl font-bold">{{ article.title }}</h1>
+    <p
+      class="text-gray-500"
+      v-if="article.content.length > 124"
+    >{{ article.content.substring(0, 124) }}...</p>
+    <p class="text-gray-500" v-else>{{ article.content }}</p>
     <div class="pt-12">
       <p class="text-gray-600">By {{ article.user }}</p>
       <p class="text-gray-600">{{ article.date_posted }}</p>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script>
